e64a9692b56491cdef69efdc081fc8997466f51f
Mohamed Badir <glury6@gmail.com>
2023-07-02 16:38:05 +0400

    Initial Commit

---
+++ About Us/About_UsApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_UsApp.swift
+//  About Us
+//
+//  Created by Mohamed Badir on 02/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct About_UsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
