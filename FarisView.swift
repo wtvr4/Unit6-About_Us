7571d6a38252085c04962e264c43a9e8fecdf879
Faris aka Animatnetic <faristheknight77@gmail.com>
2023-07-02 18:06:49 +0400

    Initialized good looking about us Page for myself (Faris)

--- FarisView.swift
+++ FarisView.swift
@@ -2,9 +2,27 @@ import SwiftUI
 
 struct FarisView: View {
     var body: some View {
-        Text("Faris View")
-            .font(.largeTitle)
-            .foregroundColor(.green)
+        ZStack {
+            Color(red: 74/255, green: 85/255, blue: 162/255)
+            
+            VStack {
+                ZStack {
+                    RadialGradient(colors: [.blue, .black, .cyan,             Color(red: 74/255, green: 85/255, blue: 162/255)], center: .center, startRadius: 0, endRadius: 190)
+                    
+                    Image("FarisPortrait")
+                        .resizable()
+                        .scaledToFit()
+                        .mask(Circle())
+                }.frame(maxWidth: 500, maxHeight: 400)
+                
+                Text("Faris Ansari")
+                    .font(
+                        .system(.largeTitle)
+                            .weight(.heavy)
+                    )
+                    .foregroundColor(Color(red: 197/255, green: 223/255, blue: 248/255))
+            }
+        }.ignoresSafeArea()
     }
 }
 
